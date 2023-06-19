3f5744997e9a26f8e484215c9b1dc76e5a7dcf0d
JonathanPA12 <jonathan.pineda.aleman@est.una.ac.cr>
2023-06-18 19:17:56 -0600

    agrego var num y concateno con string

--- src/com/welcome/home/welcome.java
+++ src/com/welcome/home/welcome.java
@@ -1,9 +1,12 @@
 package com.welcome.home;
 
 public class welcome {
+	
 
 	public static void main(String[] args) {
+		  int edad = 25;
 		System.out.println("Welcome, user!");
+		System.out.println("La edad es "+ edad + ", Felicitaciones!");
 
 	}
 
