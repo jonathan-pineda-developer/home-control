15099e1469a6beeea5867beb5585fe367d6ade36
JonathanPA12 <jonathan.pineda.aleman@est.una.ac.cr>
2023-06-18 19:34:28 -0600

    agrego conversores de variables

--- src/com/welcome/home/welcome.java
+++ src/com/welcome/home/welcome.java
@@ -7,6 +7,15 @@ public class welcome {
 		  int edad = 25;
 		System.out.println("Welcome, user!");
 		System.out.println("La edad es "+ edad + ", Felicitaciones!");
+		
+		int cantidad = 100;
+		int divisor = 3;
+		
+		double resultado = (double) cantidad /divisor;
+		System.out.println(resultado);
+		
+		String caracter = String.valueOf(cantidad) + divisor;
+		System.out.print(caracter);
 
 	}
 
