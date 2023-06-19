0a479a97e0f6e1f9f0bd8c0074f205e78302504c
JonathanPA12 <jonathan.pineda.aleman@est.una.ac.cr>
2023-06-18 19:50:33 -0600

    implemento uso de condicionales

--- src/com/welcome/home/welcome.java
+++ src/com/welcome/home/welcome.java
@@ -1,4 +1,5 @@
 package com.welcome.home;
+import java.util.Random;
 
 public class welcome {
 	
@@ -15,7 +16,26 @@ public class welcome {
 		System.out.println(resultado);
 		
 		String caracter = String.valueOf(cantidad) + divisor;
-		System.out.print(caracter);
+		System.out.println(caracter);
+		
+
+		//generar un numero random
+		Random random = new Random();
+		int randomNumber = random.nextInt(10);
+		int randomNumberbyMath = 10 + (int)  (Math.random() * 20);
+
+		System.out.println(randomNumber + " number by random");
+		System.out.println(randomNumberbyMath + " number by math");
+		
+		if(randomNumberbyMath > 20 ) {
+			System.out.println("This number "+ randomNumberbyMath +" is bigger than 20");
+		}else if(randomNumber < 10) {
+			System.out.println("This number "+ randomNumber  +" is smaller than 10");
+		} else if(randomNumber == 7 && randomNumberbyMath == 19) {
+			System.out.println("OMG What a casuality! The number "+ randomNumber  +" is equal to 7 and the number "
+					+  randomNumberbyMath + " is equal to 19");
+		} 
+
 
 	}
 
